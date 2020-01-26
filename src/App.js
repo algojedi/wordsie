1557b958553fb49c920ce58b2e28f3cb6f6687ad
banwsukh <ssbanwait78@gmail.com>
2020-01-25 20:52:29 -0500

    tokens working

--- src/App.js
+++ src/App.js
@@ -15,34 +15,67 @@ function App() {
   const context = useContext(AuthContext);
 
   useEffect(() => {
-    const token = window.sessionStorage.getItem('token');
-    
+    const token = window.sessionStorage.getItem("token");
     const url = "http://localhost:3001/login"; //TODO: change when deploying
-    
+    const getUsrUrl = "http://localhost:3001/profile/"; //TODO: change when deploying
+
     if (token) {
-      fetch(url, {
-        method: 'post',
+      console.log(token)
+      // fetch(url, {
+      //   method: "post",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //     "Authorization": token
+      //   }
+      // })
+      axios({
+        url,
+        method: "post",
         headers: {
-          'Content-type:': 'application/json',
-          'Authorization': token
+          "Content-Type": "application/json",
+          "Authorization": token
         }
       })
-      .then(resp => { 
-        console.log('resp received by useEffect', resp);
-        if (resp.data) { //TODO: this needs to fetch user from another endpoint
-          context.setAuthenticated(true);
-          context.setUser(resp.data);
-          //console.log('user set from useEffect: ', resp.data);
-        } else {
-          context.setAuthenticated(false);
-          console.log('auth denied from useEffect');
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        //'response' in err ? console.log(err.response.data.message) : 
-      })
-    }  
+        .then(resp => {
+          if (resp.data) {
+            //token exists in DB   
+            //fetch user from another endpoint
+            
+            fetch(getUsrUrl + resp.data.userId, { //fetch is working fine
+              method: "get",
+              headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+              }
+            })
+
+            // axios({  //axios not working for some reason
+            //   url: getUsrUrl + resp.data.userId,
+            //   method: "get",
+            //   headers: {
+            //     "Content-Type": "application/json",
+            //     "Authorization": token
+            //   }
+            // })
+              .then(data => data.json())
+              .then(user => {
+                console.log("user from fetch", user);
+                if (user && user.email) {
+                  context.setAuthenticated(true);
+                  context.setUser(user);
+                  //console.log('user set from useEffect: ', resp.data);
+                } else {
+                  context.setAuthenticated(false);
+                  console.log("auth denied from useEffect");
+                }
+              });
+          }
+        })
+        .catch(err => {
+          console.log('error when trying to fetch: ',err);
+
+        });
+    }
   },[])
   
   console.log('context authenticated?: ', context.authenticated);
