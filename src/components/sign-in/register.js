9ccb282dad8b80636488f4128d87081656991c54
banwsukh <ssbanwait78@gmail.com>
2020-02-26 14:48:52 -0500

    register fixed

--- src/components/sign-in/register.js
+++ src/components/sign-in/register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext } from "react";
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -13,8 +13,9 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
 import Copyright from "../copyright/copyright";
+import AuthContext from "../../contexts/authContext";
+import axios from "axios";
 
 
 
@@ -39,99 +40,134 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-const Register = () => {
-    const classes = useStyles();
-    
+const saveTokenInSession = token => {
+  window.sessionStorage.setItem("token", token);
+};
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign up
+const Register = ({ history }) => {
+  const classes = useStyles();
+
+  //const SignIn = ({ history }) => {
+  const context = useContext(AuthContext);
+
+  const url = "http://localhost:3001/register";
+
+  //const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      //const result = await axios.post(url, { name: 'Jake', email, password }); //for registering
+      const result = await axios.post(url, { name, email, password });
+      console.log("response received in sign in page ", result);
+      //if (result.data && result.data.success) {
+      if (result.status === 200) {
+        context.setAuthenticated(true);
+        history.push("/main");
+        //context.setUser(result.data);
+        //TODO : so far only sign in page receiving tokens ....saveTokenInSession(result.data.token)
+      } else {
+        //TODO: password/email didn't match
+        context.setAuthenticated(false);
+      }
+    } catch (err) {
+      console.log(err);
+      //'response' in err ? console.log(err.response.data.message) : console.log(err);
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
         </Typography>
-                <form className={classes.form} noValidate>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                autoComplete="fname"
-                                name="firstName"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="firstName"
-                                label="First Name"
-                                autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="lname"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                label="I want to receive inspiration, marketing promotions and updates via email."
-                            />
-                        </Grid>
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Sign Up
+        <form className={classes.form} onSubmit={handleSubmit} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={12}>
+              <TextField
+                autoComplete="name"
+                name="name"
+                variant="outlined"
+                required
+                fullWidth
+                id="name"
+                label="Name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                autoFocus
+              />
+            </Grid>
+            {/* <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                />
+              </Grid> */}
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                value={email}
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={e => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
           </Button>
-                    <Grid container justify="flex-end">
-                        <Grid item>
-                            
-                            <Link variant="body2" component={RouterLink} to="/">
-                                Already have an account? Sign in
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-            <Box mt={5}>
-                <Copyright />
-            </Box>
-        </Container>
-    );
-}
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link variant="body2" component={RouterLink} to="/">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+};
 
 export default Register;
